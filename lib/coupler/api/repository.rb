b0fe8392504886d8ac4b7b76043f1687bde17b85
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2016-01-14 15:55:53 -0600

    Add find and create methods to Repository

--- lib/coupler/api/repository.rb
+++ lib/coupler/api/repository.rb
@@ -4,6 +4,18 @@ module Coupler
       def initialize(adapter)
         @adapter = adapter
       end
+
+      def self.dependencies
+        ['adapter']
+      end
+
+      def find
+        @adapter.find(@name)
+      end
+
+      def create(data)
+        @adapter.create(@name, data)
+      end
     end
   end
 end
