89221d792d4065768d1dc0cb31c986abd2b77d95
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2016-01-14 15:55:16 -0600

    Add migrate, create, and index methods for SequelAdapter

--- lib/coupler/api/adapters/sequel_adapter.rb
+++ lib/coupler/api/adapters/sequel_adapter.rb
@@ -1,16 +1,32 @@
+require 'sequel'
+
 module Coupler
   module API
     class SequelAdapter
-      def intialize(options)
+      def initialize(options)
         @options = options
+      end
+
+      def migrate(path)
+        Sequel.extension(:migration)
+        Sequel::Migrator.apply(db, path)
+      end
+
+      def find(name)
+        db[name].all
+      end
+
+      def create(name, data)
+        db[name].insert(data)
       end
 
       private
 
       def db
         if @db.nil?
-          @db = Sequel.connect(options)
+          @db = Sequel.connect(@options)
         end
+        @db
       end
     end
   end
