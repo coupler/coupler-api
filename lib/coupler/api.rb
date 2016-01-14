c3a38b867bb44905701bd8b76c547b494796ff40
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2016-01-14 16:00:15 -0600

    Setup autoloading in API module

--- lib/coupler/api.rb
+++ lib/coupler/api.rb
@@ -1,11 +1,30 @@
 require "thor"
 require "rack"
-
-require "coupler/api/version"
-require "coupler/api/application"
-require "coupler/api/cli"
+require "json"
 
 module Coupler
   module API
+    autoload :Application, "coupler/api/application"
+    autoload :CLI, "coupler/api/cli"
+    autoload :Injector, "coupler/api/injector"
+    autoload :Repository, "coupler/api/repository"
+
+    # adapters
+    autoload :SequelAdapter, "coupler/api/adapters/sequel_adapter"
+
+    # repositories
+    autoload :DatasetRepository, "coupler/api/repositories/dataset_repository"
+
+    # actions
+    autoload :Datasets, "coupler/api/actions/datasets"
+
+    # routers
+    autoload :DatasetRouter, "coupler/api/routers/dataset_router"
+
+    # controllers
+    autoload :DatasetController, "coupler/api/controllers/dataset_controller"
+
+    # params
+    autoload :DatasetParams, "coupler/api/params/dataset_params"
   end
 end
