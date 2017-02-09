5c638a1512da292bf0387f93cc96c5e2999e7617
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2017-02-09 12:29:51 -0600

    Convert other repositories to Repository subclasses

--- lib/coupler-api/repositories/dataset_repository.rb
+++ lib/coupler-api/repositories/dataset_repository.rb
@@ -1,50 +1,9 @@
 module CouplerAPI
-  class DatasetRepository < ROM::Repository
-    relations :datasets
-
-    def self.dependencies
-      ['container']
-    end
-
-    def initialize(env, options = {})
+  class DatasetRepository < Repository
+    def initialize(*args)
       super
-      @create = env.commands[:datasets][:create]
-      @update = env.commands[:datasets][:update]
-      @delete = env.commands[:datasets][:delete]
-    end
-
-    def find
-      datasets.to_a.collect do |obj|
-        instantiate(obj)
-      end
-    end
-
-    def first(conditions)
-      obj = datasets.where(conditions).one
-      instantiate(obj)
-    end
-
-    def create(data)
-      obj = @create.call([data]).one
-      instantiate(obj)
-    end
-
-    def update(id, data)
-      @update.by_id(id).call(data).length
-    end
-
-    def delete(id)
-      @delete.by_id(id).call.length
-    end
-
-    private
-
-    def instantiate(obj)
-      if obj.nil?
-        nil
-      else
-        Dataset.new(obj.to_h)
-      end
+      @name = :datasets
+      @constructor = Dataset
     end
   end
 end
