9c5ecf860a52a31daa8922f2260bee13aa4cc3cf
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2017-04-12 15:17:02 -0500

    Include child entities in result of Linkage#to_h

--- lib/coupler-api/entities/linkage.rb
+++ lib/coupler-api/entities/linkage.rb
@@ -7,5 +7,14 @@ module CouplerAPI
     :dataset_2_id,
   ]) do
     attr_accessor :dataset_1, :dataset_2, :jobs, :comparators
+
+    def to_h
+      result = super
+      result['dataset_1'] = dataset_1.to_h if dataset_1
+      result['dataset_2'] = dataset_2.to_h if dataset_2
+      result['jobs'] = jobs.collect(&:to_h) if jobs
+      result['comparators'] = comparators.collect(&:to_h) if comparators
+      result
+    end
   end
 end
