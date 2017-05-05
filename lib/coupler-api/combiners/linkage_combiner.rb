ba7324b4ffe063a6b8d223c5b2aac048862198d7
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2017-05-05 11:35:10 -0500

    Update LinkageCombiner to include LinkageResults, add options

--- lib/coupler-api/combiners/linkage_combiner.rb
+++ lib/coupler-api/combiners/linkage_combiner.rb
@@ -1,36 +1,58 @@
 module CouplerAPI
   class LinkageCombiner
     def self.dependencies
-      ['LinkageRepository', 'DatasetRepository', 'ComparatorRepository', 'JobRepository']
+      [
+        'LinkageRepository', 'DatasetRepository', 'ComparatorRepository',
+        'JobRepository', 'LinkageResultRepository'
+      ]
     end
 
-    def initialize(linkage_repo, dataset_repo, comparator_repo, job_repo)
+    def initialize(linkage_repo, dataset_repo, comparator_repo, job_repo, linkage_result_repo)
       @linkage_repo = linkage_repo
       @dataset_repo = dataset_repo
       @comparator_repo = comparator_repo
       @job_repo = job_repo
+      @linkage_result_repo = linkage_result_repo
     end
 
-    def find(conditions)
+    def find(conditions, which = :all)
+      if which != :all && !which.is_a?(Array)
+        raise "'which' must be :all or an array"
+      end
+
       linkage = @linkage_repo.first(conditions)
       if linkage.nil?
         return nil
       end
 
-      dataset_1 = @dataset_repo.first({ :id => linkage.dataset_1_id })
-      if linkage.dataset_1_id == linkage.dataset_2_id
-        dataset_2 = dataset_1
-      else
-        dataset_2 = @dataset_repo.first({ :id => linkage.dataset_2_id })
+      if which == :all || which.include?(:dataset_1)
+        dataset_1 = @dataset_repo.first({ :id => linkage.dataset_1_id })
+        linkage.dataset_1 = dataset_1
       end
 
-      comparators = @comparator_repo.find({ :linkage_id => linkage.id })
-      jobs = @job_repo.find({ :linkage_id => linkage.id })
+      if which == :all || which.include?(:dataset_2)
+        if linkage.dataset_1_id == linkage.dataset_2_id
+          dataset_2 = dataset_1
+        else
+          dataset_2 = @dataset_repo.first({ :id => linkage.dataset_2_id })
+        end
+        linkage.dataset_2 = dataset_2
+      end
 
-      linkage.dataset_1 = dataset_1
-      linkage.dataset_2 = dataset_2
-      linkage.comparators = comparators
-      linkage.jobs = jobs
+      if which == :all || which.include?(:comparators)
+        comparators = @comparator_repo.find({ :linkage_id => linkage.id })
+        linkage.comparators = comparators
+      end
+
+      if which == :all || which.include?(:jobs)
+        jobs = @job_repo.find({ :linkage_id => linkage.id })
+        jobs.each do |job|
+          if job.linkage_result_id
+            job.linkage_result = @linkage_result_repo.first({ :id => job.linkage_result_id })
+          end
+        end
+        linkage.jobs = jobs
+      end
 
       linkage
     end
