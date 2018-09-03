004d744df9c5a08fb75a12cf3dee21631545b38b
Jeremy Stephens <jeremy.f.stephens@vumc.org>
2018-09-03 14:57:19 -0500

    Use MigrationCombiner in migration show action

--- lib/coupler-api/actions/migrations/show.rb
+++ lib/coupler-api/actions/migrations/show.rb
@@ -1,19 +1,19 @@
 module CouplerAPI
   module Migrations
     class Show
-      def initialize(repo, validator)
-        @repo = repo
+      def initialize(combiner, validator)
+        @combiner = combiner
         @validator = validator
       end
 
       def self.dependencies
-        ['MigrationRepository', 'MigrationValidators::Show']
+        ['MigrationCombiner', 'MigrationValidators::Show']
       end
 
       def run(params)
         errors = @validator.validate(params)
         if errors.empty?
-          migration = @repo.first(id: params[:id])
+          migration = @combiner.find(id: params[:id])
           if migration.nil?
             { 'errors' => 'not found' }
           else
